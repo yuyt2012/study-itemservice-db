27b5e7006e023bf5f2509824746600e691594720
yuyt2012 <yuyt2012@gmail.com>
2023-12-05 13:24:18 +0900

    jpa 적용

--- src/main/java/hello/itemservice/domain/Item.java
+++ src/main/java/hello/itemservice/domain/Item.java
@@ -1,14 +1,25 @@
 package hello.itemservice.domain;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import lombok.Data;
 
 @Data
+@Entity
 public class Item {
 
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "item_name", length = 10)
     private String itemName;
+    @Column(name = "price")
     private Integer price;
+    @Column(name = "quantity")
     private Integer quantity;
 
     public Item() {
